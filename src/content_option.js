1e86408e945d8e726412336d0eb816941d45bf65
Jeremy Asirwaad <jeremyasirwaad@Jeremys-MacBook-Air.local>
2022-07-14 02:51:01 +0530

    work done

--- src/content_option.js
+++ src/content_option.js
@@ -54,95 +54,71 @@ const skills = [
 	{
 		name: "Flutter",
 		value: 85
+	},
+	{
+		name: "MongoDB",
+		value: 85
+	},
+	{
+		name: "GitHub",
+		value: 90
 	}
 ];
 
 const services = [
 	{
-		title: "Web-Development",
-		description:
-			"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin."
+		title: "Web-Development - MERN stack"
+		// description:
+		// 	"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin."
 	},
 	{
-		title: "Mobile Apps",
-		description:
-			"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin."
+		title: "Mobile Apps - Flutter"
+		// description:
+		// 	"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin."
 	}
 ];
 
 const dataportfolio = [
 	{
-		img: "https://picsum.photos/400/?grayscale",
-		desctiption:
-			"The wisdom of life consists in the elimination of non-essentials.",
-		link: "#"
+		img: "https://firebasestorage.googleapis.com/v0/b/startup-st.appspot.com/o/Screenshot%202022-07-14%20at%201.50.25%20AM.png?alt=media&token=a35bbccf-4ccd-46a5-a5c3-c37316725248",
+		desctiption: "StartUp Gateway",
+		link: "https://startupgateway.pages.dev/"
 	},
 	{
-		img: "https://picsum.photos/400/800/?grayscale",
+		img: "https://firebasestorage.googleapis.com/v0/b/startup-st.appspot.com/o/Screenshot%202022-07-14%20at%201.53.02%20AM.png?alt=media&token=51bf12fd-c144-4fd1-b3f3-df9af1281f5f",
 		desctiption:
-			"The wisdom of life consists in the elimination of non-essentials.",
-		link: "#"
+			" A professional looking full stack MERN project, where employers can post jobs and students can check for job opportunities",
+		link: "https://gct-frontend.pages.dev/"
 	},
 	{
-		img: "https://picsum.photos/400/?grayscale",
-		desctiption:
-			"The wisdom of life consists in the elimination of non-essentials.",
-		link: "#"
+		img: "https://firebasestorage.googleapis.com/v0/b/startup-st.appspot.com/o/Screenshot%202022-07-14%20at%202.26.05%20AM.png?alt=media&token=e06ba03d-3d01-46b0-8140-a70c3ae1e3d4",
+		desctiption: "Flutter Dashboard App",
+		link: "https://github.com/jeremyasirwaad/laptopapp"
 	},
 	{
-		img: "https://picsum.photos/400/600/?grayscale",
-		desctiption:
-			"The wisdom of life consists in the elimination of non-essentials.",
-		link: "#"
+		img: "https://firebasestorage.googleapis.com/v0/b/startup-st.appspot.com/o/Screenshot%202022-07-14%20at%201.53.49%20AM.png?alt=media&token=caf821b4-e47b-4a49-b809-eaf11b2acb59",
+		desctiption: "StartUp Stories - Inspiring startup stories from GCT",
+		link: "https://startupgatewaystories.pages.dev/"
 	},
 	{
-		img: "https://picsum.photos/400/300/?grayscale",
-		desctiption:
-			"The wisdom of life consists in the elimination of non-essentials.",
-		link: "#"
+		img: "https://firebasestorage.googleapis.com/v0/b/startup-st.appspot.com/o/Screenshot%202022-07-14%20at%201.59.33%20AM.png?alt=media&token=67bc1d6f-fc0a-4ed2-8e6a-7df05ffd5593",
+		desctiption: "StartTalent - Dashboard to manage internship profiles",
+		link: "https://starttalent.pages.dev/"
 	},
 	{
-		img: "https://picsum.photos/400/700/?grayscale",
-		desctiption:
-			"The wisdom of life consists in the elimination of non-essentials.",
-		link: "#"
-	},
-
-	{
-		img: "https://picsum.photos/400/600/?grayscale",
-		desctiption:
-			"The wisdom of life consists in the elimination of non-essentials.",
-		link: "#"
+		img: "https://firebasestorage.googleapis.com/v0/b/startup-st.appspot.com/o/Screenshot%202022-07-14%20at%201.55.34%20AM.png?alt=media&token=62b7588a-93d3-4cdd-b2e4-c05a8af2fb0c",
+		desctiption: "React - Movie App",
+		link: "https://movieapp-83q.pages.dev/"
 	},
 	{
-		img: "https://picsum.photos/400/300/?grayscale",
-		desctiption:
-			"The wisdom of life consists in the elimination of non-essentials.",
-		link: "#"
+		img: "https://firebasestorage.googleapis.com/v0/b/startup-st.appspot.com/o/WhatsApp%20Image%202022-07-14%20at%201.52.38%20AM.jpeg?alt=media&token=d19d43a7-083d-45e3-8436-b53a1ff1ba78",
+		desctiption: "StepUp (Mobile View Only)",
+		link: "https://stepup.pages.dev/home"
 	},
 	{
-		img: "https://picsum.photos/400/?grayscale",
-		desctiption:
-			"The wisdom of life consists in the elimination of non-essentials.",
-		link: "#"
-	},
-	{
-		img: "https://picsum.photos/400/550/?grayscale",
-		desctiption:
-			"The wisdom of life consists in the elimination of non-essentials.",
-		link: "#"
-	},
-	{
-		img: "https://picsum.photos/400/?grayscale",
-		desctiption:
-			"The wisdom of life consists in the elimination of non-essentials.",
-		link: "#"
-	},
-	{
-		img: "https://picsum.photos/400/700/?grayscale",
-		desctiption:
-			"The wisdom of life consists in the elimination of non-essentials.",
-		link: "#"
+		img: "https://firebasestorage.googleapis.com/v0/b/startup-st.appspot.com/o/Screenshot%202022-07-14%20at%202.42.29%20AM.png?alt=media&token=cb531ef8-2aa0-41ca-9e7e-a53b3ff0b66e",
+		desctiption: "Weather App - Vennila Js - Enable Location Services",
+		link: "https://mystifying-austin-982566.netlify.app/"
 	}
 ];
 
